1d4151d606d35d7c27036d7435e9b1a763855072
Vlad Makarenko <vladesayl12345@gmail.com>
2020-01-15 14:42:24 +0200

    finished 4th step

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function run()
+function run() //Weclome function
 {
- line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Game!');
     $name = \cli\prompt('May I have your name?');
- line("Hello, %s!", $name);
+    line("Hello, %s!", $name);
 }
